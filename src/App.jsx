ebbf0e8226fd69d027d364bea3180ac39189a7d0
Manmeet567 <manmeet567singh@gmail.com>
2025-01-31 15:11:26 +0530

    Fix: Imported navigate from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import PublicRoute from "./components/PrivateRoute/PublicRoute";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
